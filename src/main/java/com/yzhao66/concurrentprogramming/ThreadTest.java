7f69ec219a50181c03f92158a4fb91d2d3db8b71
yangzhuohao <yangzhuohao@cecii.cn>
2021-04-24 10:23:41 +0800

    增加callable

--- src/main/java/com/yzhao66/concurrentprogramming/ThreadTest.java
+++ src/main/java/com/yzhao66/concurrentprogramming/ThreadTest.java
@@ -1,5 +1,9 @@
 package com.yzhao66.concurrentprogramming;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 public class ThreadTest {
 
     public static class MyThead extends Thread{
@@ -15,13 +19,26 @@ public class ThreadTest {
             System.out.println("i am a child thread 2");
         }
     }
+    public static class CallerTask implements Callable<String>{
 
-    public static void main(String[] args) {
+        @Override
+        public String call() throws Exception {
+            return "hello";
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         MyThead thead=new MyThead();
         thead.start();
 
         RunableTask task=new RunableTask();
         new Thread(task).start();
         new Thread(task).start();
+
+        FutureTask<String> futureTask=new FutureTask<>(new CallerTask());
+        // 启动线程
+        new Thread(futureTask).start();
+       String result =futureTask.get();
+        System.out.println(result);
     }
 }
